67548944f48d619cf310ca218d179f627972c860
devJSWC <devjswc@gmail.com>
2025-04-18 20:04:38 +0700

    Add files via upload

---
+++ android/src/main/java/com/reactnativedetector/DetectorModule.kt
@@ -0,0 +1,30 @@
+package com.reactnativedetector
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.modules.core.DeviceEventManagerModule
+
+class DetectorModule(val reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext), ScreenshotDetectionListener {
+
+    private val screenshotDetectionDelegate = ScreenshotDetectionDelegate(reactContext, this)
+
+    override fun getName() = "Detector"
+
+    @ReactMethod
+    fun startScreenshotDetection() {
+        screenshotDetectionDelegate.start()
+    }
+
+    @ReactMethod
+    fun stopScreenshotDetection() {
+        screenshotDetectionDelegate.stop()
+    }
+
+    override fun onScreenCaptured() {
+        reactContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+            .emit("UIApplicationUserDidTakeScreenshotNotification", null)
+    }
+}
