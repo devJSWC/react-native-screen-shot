55725633caf311bdbe359b4d3cb6e31fc2f67ab9
devJSWC <devjswc@gmail.com>
2025-04-19 06:04:30 +0700

    Delete lib directory

--- lib/typescript/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type Unsubscribe = () => void;
-export declare const startScreenshotDetection: () => void;
-export declare const stopScreenshotDetection: () => void;
-export declare const addScreenshotListener: (listener: () => void) => Unsubscribe;
-export {};
