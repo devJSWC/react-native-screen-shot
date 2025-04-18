4b577d5253117f3ee71ec5a3b4fef562925fe210
devJSWC <devjswc@gmail.com>
2025-04-19 01:53:39 +0700

    Delete lib directory

--- lib/typescript/src/bk/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare type Unsubscribe = () => void;
-export declare const addScreenshotListener: (listener: () => void) => Unsubscribe;
-export {};
--- lib/typescript/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Unsubscribe = () => void;
-export declare const startScreenshotDetection: () => void;
-export declare const stopScreenshotDetection: () => void;
-export declare const startScreenrecordDetection: () => void;
-export declare const stopScreenrecordDetection: () => void;
-export declare const isScreenRecording: () => Promise<boolean | undefined>;
-export declare const addScreenshotListener: (listener: () => void) => Unsubscribe;
-export declare const addScreenrecordListener: (listener: () => void) => Unsubscribe;
-export {};
